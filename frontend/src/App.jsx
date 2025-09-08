6ccd4dd69bb9be249db18894c2d25965ea7e5bf2
biruk1122 <birukfikadu29@gmail.com>
2025-09-08 16:32:55 +0300

    create home page and componenets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import Sidebar from "./components/Sidebar"
+import ChatBox from "./components/ChatBox"
+import Credits from "./pages/Credits"
+import Community from "./pages/Community"
+import { Route, Routes } from "react-router-dom"
+
 function App() {
-  return <div>App</div>
+  return (
+    <>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
