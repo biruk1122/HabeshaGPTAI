8176c80b4c78d29c10fbddc19eed34d0c59c5f32
biruk1122 <birukfikadu29@gmail.com>
2026-01-30 10:08:42 +0300

    adding loding to the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,12 @@ import { Route, Routes, useLocation } from "react-router-dom"
 import { useState } from "react"
 import { assets } from "./assets/assets"
 import './assets/prism.css';
+import { useAppContext } from "./contexts/AppContext"
+import Login from "./pages/Login"
 
 function App() {
+
+  const {user} = useAppContext()
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const {pathname} = useLocation();
@@ -18,6 +22,8 @@ function App() {
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} alt="Menu Icon" className="absolute top-3 left-3 w-6 h-6 cursor-pointer md:hidden not-dark:invert" onClick={() => setIsMenuOpen(true)}/>}
+
+    {user ? (
       <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
@@ -28,6 +34,8 @@ function App() {
           </Routes>
         </div>
       </div>
+    ):(<div className="bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen"><Login/></div>)}
+      
     </>
   )
 }
