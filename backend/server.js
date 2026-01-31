f136749d293247412aa5f3666f5ad2f88a417cfe
biruk1122 <birukfikadu29@gmail.com>
2026-01-31 11:49:15 +0300

    adding routes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config.js';
+import connectDB from './configs/db.js';
+import userRoutes from './routes/userRoutes.js';
+import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
+
+await connectDB()
 
 // Middleware
 app.use(cors());
@@ -10,6 +15,8 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => res.send('HabeshaGPTAI Backend is running'));
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
