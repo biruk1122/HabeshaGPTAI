d2907941609c23d8703e2048890edcd24a2f6e91
biruk1122 <birukfikadu29@gmail.com>
2026-02-05 17:01:44 +0300

    integrating the api key to the message controller

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,4 +1,5 @@
 import Chat from "../models/Chat.js";
+import User from "../models/User.js";
 
 // AI Text-based message controller
 export const textMessageController = async(req, res) => {
@@ -8,6 +9,43 @@ export const textMessageController = async(req, res) => {
 
         const chat = await Chat.findOne({userId, _id: chatId})
         chat.messages.push({role: 'user', content: prompt, timestamp: Date.now(), isImage: false})
+
+        const {choices} = await openai.chat.completions.create({
+            model: "gpt-4.1-mini",
+            messages: [
+                
+                {
+                    role: 'user', 
+                    content: prompt
+                },
+        
+            ]
+        });
+
+        const reply={...choices[0].message, timestamp: Date.now(), isImage: false}
+        res.json({success: true, reply})
+
+        chat.messages.push(reply)
+        await chat.save();
+        await User.updateOne({_id: userId}, )
+
     }catch(error){
+        res.json({sucess: false, message: error.message})
     }
 }
+
+// AI Image-based message controller
+export const imageMessageController = async(req, res) => {
+    try{
+        const userId = req.user.id;
+        if (req.user.credits < 2){
+            return res.json({success: false, message: "You don't have enough credits"})
+        }
+
+        const {prompt, chatId, isPublished} = req.body;
+        const chat = await Chat.findOne({userId, _id: chatId})
+
+        chat.messages.push({role: 'user', content: prompt, timestamp: Date.now(), isImage: false})
+        }catch(error){
+        }
+    }
