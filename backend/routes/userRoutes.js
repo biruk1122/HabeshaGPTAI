44d9acc0ebebe6b03a69b954e0261078a4457661
biruk1122 <birukfikadu29@gmail.com>
2026-02-10 11:50:39 +0300

    installing packages and updating routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserProfile, loginUser, registerUser } from '../controllers/userController.js';
+import { getPublishedImages, getUserProfile, loginUser, registerUser } from '../controllers/userController.js';
 import { protect } from '../middlewares/auth.js';
 
 
@@ -8,5 +8,6 @@ const userRoutes = express.Router();
 userRoutes.post('/register', registerUser)
 userRoutes.post('/login', loginUser)
 userRoutes.get('/data', protect, getUserProfile)
+userRoutes.get('/published-images', getPublishedImages)
 
 export default userRoutes;
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ import connectDB from './configs/db.js';
 import userRoutes from './routes/userRoutes.js';
 import chatRoutes from './routes/chatRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
+import creditRouter from './routes/creditRoutes.js';
+import { stripewebwooks } from './controllers/webhooks.js';
 
 const app = express();
 
 await connectDB()
+
+//Stripe Webhooks
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripewebwooks)
 
 // Middleware
 app.use(cors());
@@ -19,6 +24,7 @@ app.get('/', (req, res) => res.send('HabeshaGPTAI Backend is running'));
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRouter)
+app.use('/api/credit', creditRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
